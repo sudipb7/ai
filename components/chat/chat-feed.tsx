745b3502b63894e255396598197e9e5ef6730c8b
Sudip Biswas <sudipbiswas2142@gmail.com>
2024-02-10 14:19:30 +0530

    fix(feed): fix prerender error

--- components/chat/chat-feed.tsx
+++ components/chat/chat-feed.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import { Message } from "ai";
 
 import { BsCheck2Circle, BsClipboard2 } from "react-icons/bs";
+import { GoDotFill } from "react-icons/go";
 
 import { useChatScroll } from "@/hooks/use-chat-scroll";
 
@@ -12,12 +13,15 @@ interface ChatFeedProps {
   isLoading: boolean;
 }
 
-export const ChatFeed = ({ messages, isLoading }: ChatFeedProps) => {
+export const ChatFeed = ({ messages = [], isLoading }: ChatFeedProps) => {
   const chatRef = React.useRef<HTMLDivElement>(null);
   const bottomRef = React.useRef<HTMLDivElement>(null);
   const [isCopied, setIsCopied] = React.useState(false);
 
-  const lastMessage = messages[messages.length - 1].content.length;
+  const lastMessage = React.useMemo(() => {
+    if (messages.length === 0) return 0;
+    return messages[messages.length - 1].content.length;
+  }, [messages]);
 
   const handleCopy = React.useCallback((message: string) => {
     navigator.clipboard.writeText(message);
@@ -41,47 +45,48 @@ export const ChatFeed = ({ messages, isLoading }: ChatFeedProps) => {
     >
       <div className="flex-1" />
       <div className="flex flex-col gap-4 w-full">
-        {messages.map((message) => {
-          const isIncomingResponse =
-            isLoading &&
-            message.role === "assistant" &&
-            message === messages[messages.length - 1];
+        {messages.length > 0 &&
+          messages.map((message) => {
+            const isIncomingResponse =
+              isLoading &&
+              message.role === "assistant" &&
+              message === messages[messages.length - 1];
 
-          return (
-            <div
-              key={message.id}
-              className={`
-              relative w-4/5 rounded-xl space-y-1 p-3 border border-[#373737] transition-all
-              ${message.role === "user" ? "ml-auto" : "mr-auto"}
-              ${message.role === "user" ? "bg-transparent" : "bg-[#292927]"}
-            `}
-            >
-              <p className="font-medium text-sm text-[#8F8F8F] capitalize transition-all">
-                {message.role === "user" ? "user" : "AI"}
-              </p>
-              <p className="text-zinc-200 text-justify tracking-wide transition-all">
-                {message.content}
-                {isIncomingResponse && (
-                  <div className="h-2 w-2 bg-white rounded-full animate-ping mt-1" />
-                )}
-              </p>
-              {message.role === "assistant" && (
-                <button
-                  type="button"
-                  aria-label="Copy message"
-                  onClick={() => handleCopy(message.content)}
-                  className="absolute bottom-0 -right-8 bg-transparent outline-none border-none w-fit p-2"
-                >
-                  {isCopied ? (
-                    <BsCheck2Circle className="h-4 w-4 text-cyan-500" />
-                  ) : (
-                    <BsClipboard2 className="h-4 w-4 text-[#8F8F8F]" />
+            return (
+              <div
+                key={message.id}
+                className={`
+                  relative w-4/5 rounded-xl space-y-1 p-3 border border-[#373737] transition-all
+                  ${message.role === "user" ? "ml-auto" : "mr-auto"}
+                  ${message.role === "user" ? "bg-transparent" : "bg-[#292927]"}
+                `}
+              >
+                <p className="font-medium text-sm text-[#8F8F8F] capitalize transition-all">
+                  {message.role === "user" ? "user" : "AI"}
+                </p>
+                <p className="text-zinc-200 text-justify tracking-wide transition-all">
+                  {message.content}
+                  {isIncomingResponse && (
+                    <GoDotFill className="h-2 w-2 bg-white rounded-full animate-ping mt-1" />
                   )}
-                </button>
-              )}
-            </div>
-          );
-        })}
+                </p>
+                {message.role === "assistant" && (
+                  <button
+                    type="button"
+                    aria-label="Copy message"
+                    onClick={() => handleCopy(message.content)}
+                    className="absolute bottom-0 -right-8 bg-transparent outline-none border-none w-fit p-2"
+                  >
+                    {isCopied ? (
+                      <BsCheck2Circle className="h-4 w-4 text-cyan-500" />
+                    ) : (
+                      <BsClipboard2 className="h-4 w-4 text-[#8F8F8F]" />
+                    )}
+                  </button>
+                )}
+              </div>
+            );
+          })}
       </div>
       <div ref={bottomRef} />
     </div>
--- components/chat/chat-form.tsx
+++ components/chat/chat-form.tsx
@@ -23,12 +23,16 @@ export const ChatForm = ({
   handleSubmit,
   handleInputChange,
 }: ChatFormProps) => {
-  // TODO: If a response is being fetched, then the form should not be submitted
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (isLoading) return;
+    handleSubmit(e);
+  };
 
   return (
     <div className="w-full sticky bottom-0 inset-x-0">
       <form
-        onSubmit={handleSubmit}
+        onSubmit={handleFormSubmit}
         className="relative p-3 border-t border-[#373737] bg-[#1F1F1D] z-[1]"
       >
         <input
@@ -42,7 +46,7 @@ export const ChatForm = ({
           className="w-full p-4 pr-12 bg-transparent text-zinc-200 rounded-full outline-none border border-[#373737] focus:border-transparent focus:ring-offset-0 focus:ring-1 focus:ring-cyan-500 transition"
         />
         <button
-          disabled={input === ""}
+          disabled={input === "" || isLoading}
           type="submit"
           aria-label="Send message"
           className="p-2 rounded-full outline-none border border-cyan-500 disabled:border-[#373737] disabled:cursor-not-allowed disabled:opacity-50 absolute top-[21.5px] right-[20px] peer bg-[#1F1F1D] z-[2] transition"
